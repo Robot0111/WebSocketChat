b9c4e7cad67935001d66c79a3ed168fe348f97b5
Johnson jeffery <Johnson.Luql@gmail.com>
2018-10-04 13:08:56 +0800

    Update WsConfig.java

--- app/src/main/java/webgroupchat/androidhive/info/chat/WsConfig.java
+++ app/src/main/java/webgroupchat/androidhive/info/chat/WsConfig.java
@@ -2,5 +2,5 @@ package webgroupchat.androidhive.info.chat;
 
 public class WsConfig {
     //public static final String URL_WEBSOCKET = "http://192.168.75.1:8080/SocketAndroid/chat?name=";
-    public static final String URL_WEBSOCKET =  "wss://luql.top/SocketAndroid/chat?name=";
+    public static final String URL_WEBSOCKET =  "wss://luql.iloveyou/SocketAndroid/chat?name=";
 }
